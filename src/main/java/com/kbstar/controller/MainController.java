85cedfb0cc4541596e8c5a6dc6546df7383dd4d0
sungyoung1107 <dong09161107@gmail.com>
2023-05-11 15:51:37 +0900

    Initial commit

---
+++ src/main/java/com/kbstar/controller/MainController.java
@@ -0,0 +1,24 @@
+package com.kbstar.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @Value("${adminserver}")
+    String adminserver;
+
+    @RequestMapping("/")
+    public String main(Model model) {
+        model.addAttribute("adminserver", adminserver);
+        return "index";
+    }
+
+}
